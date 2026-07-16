9ece4097c48377f04e8b9707d7c36a5135be6976
Ira Lisovska <lisovskaya_irina@list.ru>
2026-07-16 23:21:38 +0300

    Added PropertyReader

--- src/main/java/api/endpoints/Endpoints.java
+++ src/main/java/api/endpoints/Endpoints.java
@@ -3,14 +3,8 @@ package api.endpoints;
 public class Endpoints {
     public static final String CASE_BY_CODE = "/case/{code}";
     public static final String CASE_BY_CODE_ID = "/case/{code}/{id}";
-    public static final String PLAN_BY_CODE = "/plan/{code}";
-    public static final String PLAN_BY_CODE_ID = "/plan/{code}/{id}";
     public static final String PROJECT = "/project";
     public static final String PROJECT_BY_CODE = "/project/{code}";
-    public static final String RUN_BY_CODE = "/run/{code}";
-    public static final String RUN_BY_CODE_ID = "/run/{code}/{id}";
-    public static final String RUN_BY_CODE_ID_PUBLIC = "/run/{code}/{id}/public";
-    public static final String RUN_BY_CODE_ID_COMPLETE = "/run/{code}/{id}/complete";
     public static final String SUITE_BY_CODE = "/suite/{code}";
     public static final String SUITE_BY_CODE_ID = "/suite/{code}/{id}";
 }
